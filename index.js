ce29e4c78bb1417f7d7d1136a40d20914efef731
Jade Huynh <35jadeh@gmail.com>
2019-07-03 20:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name)
+{
+  return kittens.unshift(name)
+}  
+
+function destructivelyRemoveLastKitten()
+{
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  return kittens.shift()
+}
+
+function appendKitten(name)
+{
+  var array2 = [name] 
+  kittens = kittens.concat(array2)
+  return kittens
+}
+
+function prependKitten(name)
+{
+  kittens = [name,...kittens]
+  return kittens
+}
+
+function removeLastKitten()
+{
+  kittens = kittens.slice(0, kittens.length - 1)
+  return kittens
+}
+
+function removeFirstKitten()
+{
+  kittens = kittens.slice(1)
+  return kittens
+}
